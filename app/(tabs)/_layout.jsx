e4e63279aa25b57cbeacff5affb29c174987645b
katemcg <katemcg@umich.edu>
2025-02-05 20:46:28 -0500

    Starter files

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,65 @@
+import { Tabs } from 'expo-router';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#8B0000',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="browse"
+        options={{
+          title: 'Browse',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'search' : 'search-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+
+
+<Tabs.Screen
+        name="library"
+        options={{
+          title: 'Library',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'library' : 'library-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlist"
+        options={{
+          title: 'Wishlist',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'list' : 'list-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+
+<Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'person-circle' : 'person-circle-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      
+    </Tabs>
+
+  );
+}
---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,12 @@
+import { Text, View,  } from "react-native";
+import {Link} from "expo-router";
+
+export default function Profile() {
+  return (
+    <View
+    >
+      <Text className = "text-red-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
+      <Link className = "bg-black text-white" href="/(tabs)">LINK TO APP</Link>
+    </View>
+  );
+}
